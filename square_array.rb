807cdd6bd876fb9985d2b807e0cec36ac0979209
79ceca7e-1710-4439-8f8b-c115f316e9e9 <github-enterprise+79ceca7e-1710-4439-8f8b-c115f316e9e9@flatironschool.com>
2020-02-10 16:32:53 -0800

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
   # your code here
+  new_array = Array.new
+  array.each { |num| new_array << num ** 2 }
+  return new_array
 end
+
+def square_array_collect(array)
+  array.collect { |num| num**2}
+end
+
+puts square_array_collect([1,2,3,4])
